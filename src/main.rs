5c15cb4bde3697fa5c3cf5a6cbe72ea2a67bb688
coin1111 <celticsnow123@gmail.com>
2022-03-13 15:04:46 -0700

    signers

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,11 @@ use serde_json::Error;
 use std::fs;
 use std::io::Read;
 use async_std::task;
+use std::convert::TryInto;
+use tokio::runtime::Runtime;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let provider = Provider::<Http>::try_from(
         "http://localhost:8545",
     )
@@ -35,26 +38,42 @@ fn main() {
     let client = validator_wallet.connect(provider);
     let bridge_escrow_contract = Contract::new(escrow_addr, abi, &client);
     println!("bridge_escrow_contract: {:?}",bridge_escrow_contract);
-    // let data = bridge_escrow_contract
-    //     .method::<_, H256>("withdrawFromEscrowThis",
-    //                        (
-    //                            //Address::from(sender_wallet.private_key()).to_string().to_owned(),
-    //                            //Address::from(receiver_wallet.private_key()).to_string().to_owned(),
-    //                            "100".to_owned(),
-    //                            "0x123".to_owned())).map_err(|e|{
-    //     println!("Error: {}",e)
-    // }).unwrap();
+    let transfer_id_str="eab47fa3a3dc42bc8cbc48c02182669e";
+    let transfer_id:[u8;16] = hex_to_bytes(&String::from(transfer_id_str)).unwrap()
+        .try_into().unwrap();
+    let balance:u64 = 10;
+    println!("{:?} {:?} {:?} {:?}",Address::from(sender_wallet.private_key()),
+             Address::from(receiver_wallet.private_key()),
+             balance,
+             transfer_id);
     let data = bridge_escrow_contract
-        .method::<_, H256>("withdrawFromEscrowThis",
-                           ("0x123".to_owned())).map_err(|e|{
+        .method::<_, ()>("withdrawFromEscrowThis",
+                           (
+                               Address::from(sender_wallet.private_key()),
+                               Address::from(receiver_wallet.private_key()),
+                               balance,
+                               transfer_id)).map_err(|e|{
         println!("Error: {}",e)
     }).unwrap();
-    task::block_on(async {
         let tx_hash = data
-            .send().await.unwrap();
+            .send().await.map_err(|e|{
+            println!("Error tx: {:?}",e)
+        }).unwrap();
         println!("withdrawFromEscrowThis: {:?}", tx_hash);
-    });
 
+    fn hex_to_bytes(s: &String) -> Option<Vec<u8>> {
+        if s.len() % 2 == 0 {
+            (0..s.len())
+                .step_by(2)
+                .map(|i| {
+                    s.get(i..i + 2)
+                        .and_then(|sub| u8::from_str_radix(sub, 16).ok())
+                })
+                .collect()
+        } else {
+            None
+        }
+    }
 }
 
 
