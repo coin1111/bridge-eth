336d6a78dae88ce931daec4758824d8392d8c8c7
coin1111 <celticsnow123@gmail.com>
2022-03-12 18:01:16 -0800

    signers

--- bridge-ethers/src/signers.rs
+++ bridge-ethers/src/signers.rs
@@ -3,20 +3,48 @@ use ethers::{
     signers::Wallet,
     types::TransactionRequest,
 };
+use std::collections::HashMap;
 use std::fs;
 use std::io::{Error, ErrorKind};
-
-pub struct Signers;
-
-impl Signers {
-    fn get_private_key(path: &str) ->Result<String, Box<Error>> {
-        let line = fs::read_to_string(path)?;
-        // line[1].split(":")[1].trim();
-        match line.split("\n").take(2).last()
-            .and_then(|s|{s.clone().split(":").take(2).last().
-                and_then(|s|{Some(s.clone().trim())})})  {
-            Some(s) => Ok(String::from(s)),
-            None => Err(Box::from(Error::new(ErrorKind::Other, "can't find private key")))
+use std::iter::FromIterator;
+use std::str::FromStr;
+pub fn get_signer(
+    signers: &HashMap<&str, ethers::signers::Wallet>,
+    nick: &str,
+) -> Result<ethers::signers::Wallet, String> {
+    if (!nick.clone().starts_with("0x")) {
+             match signers.get(&nick as &str)  {
+            Some(w) => Ok(w.clone()),
+            None => Err(format!("user {} is not found",nick))
         }
+    } else {
+        ethers::signers::Wallet::from_str(&nick).map_err(|e|{e.to_string()})
     }
+}
+
+fn get_private_key(path: &str) -> Result<String, String> {
+    let line = fs::read_to_string(path).map_err(|e| e.to_string())?;
+    // line[1].split(":")[1].trim();
+    match line.split("\n").take(2).last().and_then(|s| {
+        s.clone()
+            .split(":")
+            .take(2)
+            .last()
+            .and_then(|s| Some(s.clone().trim()))
+    }) {
+        Some(s) => Ok(String::from(s)),
+        None => Err(format!("can't find private key: {}", path)),
+    }
+}
+pub fn get_signers() -> Result<HashMap<&'static str, ethers::signers::Wallet>, String> {
+    let names = vec!["alice", "bob", "carol", "pete", "todd", "bridgeEscrow"];
+    let mut pairs: Vec<(&str, ethers::signers::Wallet)> = Vec::new();
+    for n in names.iter() {
+        let key = get_private_key(&format!("accounts/{}.txt", n))?;
+        let w = ethers::signers::Wallet::from_str(&key).map_err(|e| e.to_string())?;
+        pairs.push((n, w));
+    }
+
+    let hm: HashMap<&str, ethers::signers::Wallet> = pairs.iter().cloned().collect();
+    Ok(hm)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ethers::{
     types::TransactionRequest,
 };
 use bridge_ethers::config;
+use bridge_ethers::signers;
 use std::convert::TryFrom;
 
 fn main() {
@@ -13,7 +14,14 @@ fn main() {
         .unwrap();
     let config = bridge_ethers::config::Config::read_config(".bridge_escrow.config").unwrap();
     let escrow_addr = config.get("escrowContract").unwrap();
-    println!("Hello, world!: {:?}",escrow_addr);
+    println!("escrow_addr: {:?}",escrow_addr);
+
+    let signers = bridge_ethers::signers::get_signers().unwrap();
+
+    let sender_wallet = bridge_ethers::signers::get_signer(&signers, &"pete");
+    let receiver_wallet = bridge_ethers::signers::get_signer(&signers, &"todd");
+    let validator_wallet = bridge_ethers::signers::get_signer(&signers, &"alice");
+
 }
 
 
