92128ff479da3cce900a4a70f8a6a5179f30ab9b
coin1111 <celticsnow123@gmail.com>
2022-03-21 20:01:34 -0700

    make pub

--- bridge-ethers/src/util.rs
+++ bridge-ethers/src/util.rs
@@ -53,7 +53,7 @@ pub struct AccountInfo {
     idx: ethers::prelude::U256,
     is_closed: bool,
 }
-fn vec_to_array<T, const N: usize>(v: Vec<T>) -> Result<[T; N], String> {
+pub fn vec_to_array<T, const N: usize>(v: Vec<T>) -> Result<[T; N], String> {
     v.try_into()
         .map_err(|v: Vec<T>| format!("Expected a Vec of length {} but it was {}", N, v.len()))
 }
